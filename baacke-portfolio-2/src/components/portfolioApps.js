a59a4561aca12e518b8919276e40afedb26ebc34
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-22 07:40:38 -0400

    started portfolio component

---
+++ baacke-portfolio-2/src/components/portfolioApps.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Container from './Container';
+import Row from './Row';
+import Col from './Col';
+import Card from './Card';
+
+return (
+    <Container>
+        <Row>
+            <Col>
+            <Card>
+
+            </Card>
+            </Col>
+        </Row>
+    </Container>
+)
+
+export default portfolioApps;
