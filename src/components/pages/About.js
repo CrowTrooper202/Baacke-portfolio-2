621912559d98d8686f42fc01106ec7f0a6afa739
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-22 09:58:02 -0400

    set up about me page

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About(){
+    return(
+        <section>
+            <h1> About me</h1>
+            <p>
+            Highly capable Non-Commissioned Officer (NCO) with recognized competencies in both oral and written
+communications. Effective multi-tasker who is highly familiar with modern project management
+techniques with a demonstrated ability to work both independently and collaboratively alongside internal
+personnel as a motivated leader. A continuous learner who regularly challenges himself and is always
+willing to learn. Currently has a Top-Secret Clearance with Sensitive Compartment Information.
+            </p>
+        </section>
+    )
+}
