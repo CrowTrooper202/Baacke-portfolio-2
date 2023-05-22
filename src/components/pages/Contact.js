f1da7a1a91e8b3e96af29583eb4bcdff347e3037
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-22 10:03:30 -0400

    set up contact and portfolio page

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Contact() {
+    <section>
+        <h1>drop me a message!</h1>
+    </section>
+}
---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Portfolio(){
+    <section>
+        <h1>here are some of me resent projects</h1>
+    </section>
+}
