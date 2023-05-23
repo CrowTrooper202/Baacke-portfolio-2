13cc528759663d94e8f9c1eed91ecc78c8c32756
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-23 05:30:57 -0400

    added portfolio container, updated navtabs to curent pages, and commented out portfolio

--- src/components/portfolio/portfolioApps.js
+++ src/components/portfolio/portfolioApps.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Container from './Container';
-import Row from './Row';
-import Col from './Col';
-import Card from './Card';
-import appDetails from './appDetails';
+// import React from 'react';
+// import Container from './Container';
+// import Row from './Row';
+// import Col from './Col';
+// import Card from './Card';
+// import appDetails from './appDetails';
 
-const portfolioContainer =() =>{
-return(
-    <Container>
-        <Row>
-            <Col size='md-4'>
-            <Card Heading = 'Text Editor'>
-                <appDetails/>
-            </Card>
-            </Col>
-        </Row>
-    </Container>
-)
-}
+// const portfolioContainer =() =>{
+// return(
+//     <Container>
+//         <Row>
+//             <Col size='md-4'>
+//             <Card Heading = 'Text Editor'>
+//                 <appDetails/>
+//             </Card>
+//             </Col>
+//         </Row>
+//     </Container>
+// )
+// }
 
-export default portfolioContainer;
+// export default portfolioContainer;
