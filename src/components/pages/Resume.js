6732b50b24980ca558d73bb0729a646037df66a0
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-23 06:13:13 -0400

    added resuem (still needs work) and instal funtionality

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
-
+import resume from '../utils/resume/Resume.pdf'
 export default function Contact() {
-    return(
-    <section>
-        <h1>here is my resume</h1>
-    </section>
-    )
+    // import Resume from '../utils/resume/Resume.pdf'
+    // const Resume = () => {
+        return (
+          <div className='main-container'>
+            <Document file={resume}>
+                <Page pageNumber={2}/>
+            </Document>
+          </div>
+        )
+    //   }
+    //   Resume()
 }
